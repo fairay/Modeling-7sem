bf4d3401ce804bb1824643c1a8d481d600b8811a
fairay <48217178+fairay@users.noreply.github.com>
2021-11-16 01:12:03 +0300

    added lab2 backend

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(543, 69);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(102, 35);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ lab2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    class Model
+    {
+        public const int maxN = 10;
+        public double[] pArr;
+        public double[] tArr;
+        public double[,] tMatrix;
+        public int n { get; }
+        public double T = 0;
+        public Model(int n_)
+        {
+            this.n = n_;
+            this.tMatrix = new double[maxN, maxN];
+            this.tArr = new double[maxN];
+            this.pArr = new double[maxN];
+            for (int i = 0; i < n; i++)
+                pArr[i] = 1.0 / n;
+        }
+
+        // returns is stabilized
+        public bool Step(double dt)
+        {
+            double[] newP = new double[maxN];
+            for (int i=0; i < n; i++)
+            {
+                newP[i] = pArr[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) continue;
+                    newP[i] += dt * (pArr[j] * tMatrix[j, i] - pArr[i] * tMatrix[i, j]);
+                }
+            }
+
+            bool isSt = IsStable(pArr, newP);
+
+            pArr = newP;
+            T += dt;
+
+            setStable();
+            return isSt;
+        }
+
+        private bool IsStable(double[] oldP, double[] newP)
+        {
+            for (int i = 0; i < n; i++)
+                if (Math.Abs(oldP[i] - newP[i]) / newP[i] > 1e-10)
+                    return false;
+            return true;
+        }
+
+        public double[] Kolmogorov()
+        {
+            double[] res = new double[maxN];
+
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = 0;
+                for (int j = 0; j < n; j++)
+                    res[i] += pArr[j] * tMatrix[j, i] - pArr[i] * tMatrix[i, j];
+            }
+
+            return res;
+        }
+
+        private void setStable()
+        {
+            double[] kArr = Kolmogorov();
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Abs(kArr[i]) < 1e-5 && tArr[i] <= 1e-7)
+                    tArr[i] = T;
+                else if (Math.Abs(kArr[i]) > 1e-5 && tArr[i] > 1e-7)
+                    tArr[i] = 0;
+            }
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Model m = new Model(6);
+
+            m.tMatrix[0, 1] = 0.1;
+            m.tMatrix[1, 2] = 0.1;
+            m.tMatrix[2, 3] = 0.1;
+            m.tMatrix[3, 4] = 0.1;
+            m.tMatrix[4, 5] = 0.1;
+
+            m.tMatrix[1, 0] = 0.1;
+            m.tMatrix[2, 1] = 0.2;
+            m.tMatrix[3, 2] = 0.3;
+            m.tMatrix[4, 3] = 0.4;
+            m.tMatrix[5, 4] = 0.5;
+
+            Console.WriteLine("[{0}]", string.Join(", ", m.pArr));
+            while (!m.Step(0.01))
+                Console.WriteLine("{0}\t[{1}]\t[{2}]", m.T, string.Join(", ", m.pArr), string.Join(", ", m.Kolmogorov()));
+            Console.WriteLine("Done: [{0}]", string.Join(", ", m.pArr));
+            Console.WriteLine("Done: [{0}]", string.Join(", ", m.tArr));
+            return;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
